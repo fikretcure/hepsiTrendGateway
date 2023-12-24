e2b6f07cf355bcb0e5a9e1cdafa2d5d50b646d7c
Fikret CURE <fikretcure@gmail.com>
2023-12-24 21:48:19 +0300

    Updated OrderController.php

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 
 use App\Enums\RolesEnum;
+use App\Http\Services\IyzicoService;
 use App\Http\Services\ShoppingService;
+use http\Env\Response;
 use Illuminate\Http\JsonResponse;
 
 /**
@@ -18,6 +20,12 @@ class OrderController extends Controller
      */
     public ShoppingService $shoppingService;
 
+
+    /**
+     * @var IyzicoService
+     */
+    public IyzicoService $iyzicoService;
+
     /**
      *
      */
@@ -25,6 +33,7 @@ class OrderController extends Controller
     {
         $this->middleware('role:' . RolesEnum::ADMIN->value)->only(['store', 'update', 'destroy']);
         $this->shoppingService = new ShoppingService();
+        $this->iyzicoService = new IyzicoService();
     }
 
 
@@ -104,5 +113,25 @@ class OrderController extends Controller
     public function removeProduct(): JsonResponse
     {
         return $this->shoppingService->send();
+    }
+
+
+    /**
+     * @return JsonResponse
+     */
+    public function payment(): JsonResponse
+    {
+        $basket = $this->shoppingService->send(method: 'get', path: 'api/basket/order-id', file_path: null);
+        request()->merge([
+            'order_id' => $basket->original['data']['order_id'],
+            'money' => $basket->original['data']['money']
+        ]);
+        $iyzico = $this->iyzicoService->send(method: 'post', path: 'api/payment', file_path: null);
+
+        if ($iyzico->original['data']['status']) {
+            return $this->shoppingService->send(method: 'post', path: 'api/basket/payment-success', file_path: null);
+
+        }
+        return $iyzico;
     }
 }
